d263eda941f02cd82b463d56c0ee6949512aac63
Thomas Wellmann da Riva Cesar <thomadastube@gmail.com>
2024-05-02 18:41:23 +0200

    gato anda/attaca

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] SpriteRenderer playerSpriteRenderer;
+    [SerializeField] Animator playerAnimator;
+    Vector2 newPosition;
+
+    void Start()
+    {
+        Debug.Log("Start Method in Player Move is being called");
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerAnimator.SetBool("attacking", false);
+        playerAnimator.SetBool("running", false);
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            playerAnimator.SetBool("running", true);
+            playerSpriteRenderer.flipX = false;
+            newPosition = transform.position;
+            newPosition.x += speed * Time.deltaTime;
+            transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerAnimator.SetBool("running", true);
+            playerSpriteRenderer.flipX = true;
+            newPosition = transform.position;
+            newPosition.x -= speed * Time.deltaTime;
+            transform.position = newPosition;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            playerAnimator.SetBool("attacking", true);
+        }
+    }
+
+}
